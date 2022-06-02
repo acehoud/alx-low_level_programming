160c5c47c87aabbd6494ed44fa38320496cc7a5e
Anele Bilikwa <acehoud>
2022-06-02 13:45:39 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *main - this is a starting point of the program
+ *Retrun: 0 if run succesful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
