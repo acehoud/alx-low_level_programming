0df297cdd949df2131884ffc805b19731f1f1388
Anele Bilikwa <acehoud>
2022-06-03 14:24:01 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,18 @@
 int main(void)
 {
 	char x = 'a';
+	char y = 'A';
 	
 	while (x <= 'z')
 	{
 		putchar(x);
 		x = x + 1;
-		
-		char y = 'A';
+	}
 
-		while (y <= 'Z')
-		{
+	while (y <= 'Z')
+	{
 			putchar(y);
 			y = y + 1;
-		}
 	}
 
 	putchar('\n');
