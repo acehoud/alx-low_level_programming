5b04e7c3db91249bafb5bd03ac40ccf6d124f124
Anele Bilikwa <acehoud>
2022-06-03 15:12:31 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start program
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x = '0';
+	
+	while (x <= 9)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+	return(0);
+}
