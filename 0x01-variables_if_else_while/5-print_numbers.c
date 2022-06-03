60e64feecf86cddc1e80aeb463384923512c3e07
Anele Bilikwa <acehoud>
2022-06-03 15:20:47 -0700

    Del

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - starting point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		x = x +1;
+	}
+
+	putchar('\n');
+	return (0);
+}
