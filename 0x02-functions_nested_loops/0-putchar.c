816e3cc67f009d06edb8d923e67d8bc5b238b261
Anele Bilikwa <acehoud>
2022-06-07 12:50:25 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - program start
+ * Return 0
+ */
+
+int main(void)
+{
+	char x[] = 
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
