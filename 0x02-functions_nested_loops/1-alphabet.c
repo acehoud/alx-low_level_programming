0a366e77eeddb54f4b2c0da45136391c476e75ea
Anele Bilikwa <acehoud>
2022-06-07 14:26:58 -0700

    void print_alphabet(void)

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prgram entry point
+ * Description: use _putchar
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char j ='a';
+
+	while (j <= 'z')
+	{
+		_putchar(j);
+		j++;
+	}
+	
+	_putchar('\n');
+
+	return (0);
+}
