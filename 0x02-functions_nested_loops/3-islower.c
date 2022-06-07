1532c4e7d934c03f20b1e6efa815a6255f812553
Anele Bilikwa <acehoud>
2022-06-07 16:11:22 -0700

    _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,27 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * int _islower - Prototype function
+ * Description: This checks between lower case
+ * Returns: 1
+ */
+
+int _islower(int c);
+
+int main(void)
+{
+	check = _islower(c);
+}
+
+int _islower(int c)
+{
+	if (c.islower)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
