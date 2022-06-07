422af14436146e72f3e87fe769484ba4aa8b9f8f
Anele Bilikwa <acehoud>
2022-06-07 16:33:43 -0700

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * int _islower - Prototype function
- * Description: This islower checks between lower case
- * @c: ASCII to be valuated
- * ReturN: 1 if successful else othwerise 0
+ * _islower - Function prototype
+ * Description: This islower checks if character is lowercase.
+ * @c: ASCII character to be valuated
+ * ReturN: 1 if lowercase else othwerise 0
  */
 
 int _islower(int c)
