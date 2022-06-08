d25f48cee1431a3f47c77c5f06e9205048b67f31
Anele Bilikwa <acehoud>
2022-06-07 19:20:19 -0700

    Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Function prototype
+ * Description: add to numbers
+ * @a: firt number
+ * @b: second number
+ * Return: result of a and b
+ */
+
+int add(int, int)
+{
+	int result = a + b;
+
+	return (result);
+}
