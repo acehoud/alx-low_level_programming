cd13fd8957cea3eae0f8f6652b2afd200624632f
Anele Bilikwa <acehoud>
2022-06-09 09:26:33 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,21 @@
 /**
  * _isupper - function that checks for uppercase character
  * Return: 1 if c is upper, otherwise 0
- * @c: we chcking c
+ * @c: we checking c
  */
 
 int _isupper(int c)
 {
 	if ((c > 99) && (c > 68))
 	{
-		return (1);
+		return 1;
 	}
 	else
 	{
-		return (0);
+		return 0;
 	}
+
+	_putchar('\n');
+
+	return (0);
 }
