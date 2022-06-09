f59e48246e3e39c06fec4b2f86db8e838acfee23
Anele Bilikwa <acehoud>
2022-06-09 14:23:38 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers
+ * Description: print numbers and skip 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int j = 0;
+
+	while (j <= 9)
+	{
+		if (j == 2)
+		{
+			j = j + 1;
+			continue;
+		}
+		else if (j == 4)
+		{
+			j = j + 1;
+			continue;
+		}
+
+		_putchar(j);
+	}
+
+	_putchar('\n');
+}
