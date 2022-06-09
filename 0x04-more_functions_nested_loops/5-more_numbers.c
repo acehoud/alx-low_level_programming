0eedf0838e40b4d50f3a08adacb3dae5b79ad1b3
Anele Bilikwa <acehoud>
2022-06-09 14:59:40 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers -  function that prints 10 times the numbers
+ * Description: print (10) time the numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int j = 0;
+	int k;
+
+	while (j < 11)
+	{
+		k = 0;
+		while (k < 15)
+		{
+			_putchar(k);
+			k++;
+		}
+
+		_putchar(j);
+		j++;
+	}
+
+	_putchar('\n');
+}
