26aacd5295f6767ad4eba98df09aba31daf71bb8
Anele Bilikwa <acehoud>
2022-06-09 19:16:01 -0700

    Test

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	unsigned long int i = 3, j 612852475143;
+	unsigned long int i = 3, n 612852475143;
 
 	for (; i < 12057; i += 2)
 	{
-		while (j % i == 0 && j != i)
-			j /= 1;
+		while (n % i == 0 && n != i)
+			n /= i;
 	}
 	printf("%lu\n", j);
 
