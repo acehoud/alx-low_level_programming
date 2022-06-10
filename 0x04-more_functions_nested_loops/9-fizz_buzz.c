077948097bde48483599d487cd4a361617f88a6d
Anele Bilikwa <acehoud>
2022-06-09 17:42:11 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,13 @@ int main(void)
 	for (; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 3 == 0)
-		{
-			printf("Fizz ")
-		}
-		else if (i % 5)
-		{
+			printf("Fizz ");
+		else if (i % 5 == 0)
 			printf("Buzz ");
-		}
 		else
-		{
-			printf("%d ", i);
-		}
+			printf("%d ", 1);
 	}
 	printf("Buzz\n");
 
