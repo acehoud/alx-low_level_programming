2d1b97333920c2c36bff679d8301c6fce624337f
Anele Bilikwa <acehoud>
2022-06-17 11:24:40 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * @str: string to encode
+ * Return: Always return 0
+ */
+
+char *leet(char *str)
+{
+	int i;
+	char lower_letter[] = "aeotl";
+	char upper_letter[] = "AEOTL";
+	char num[] = "43071";
+
+	for (i = 0; str[i]; i++)
+	{
+		int j;
+
+		for (j = 0; x < 5; j++)
+		{
+			if (str[i] != lower_letter[j] && str[i] != lower_letter[j])
+				continue;
+
+			str[i] = num[j];
+			break;
+		}
+	}
+
+	return (str);
+}
