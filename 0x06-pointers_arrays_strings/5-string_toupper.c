0c7840873df6b77b0b406bd08c01216269bded0e
Anele Bilikwa <acehoud>
2022-06-17 10:29:54 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a string to uppercase
+ * @str: string to be converted
+ * Return: Always return 0
+ */
+
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+	}
+
+	return (str);
+}
