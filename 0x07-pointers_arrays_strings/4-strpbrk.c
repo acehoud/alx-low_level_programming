d85711b5189fdfc5183a063c84d63bf1594de2cd
Anele Bilikwa <acehoud>
2022-06-20 13:10:34 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,12 +6,13 @@
  * @s: This is the C string to be scanned
  * @accept: This is the C string containing the characters to match
  * Return: Always return 0
+ * or NULL
  */
 
 char *_strpbrk(char *s, char *accept)
 {
 	int i;
-	
+
 	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
