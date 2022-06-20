16e8f9991335d444d8fc6caf22fa0b1e0f48a0f3
Anele Bilikwa <acehoud>
2022-06-20 12:44:33 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,12 +17,11 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] > '\0'; j++)
 		{
-			while (s[i] != accept[j])
-			{
+			if (s[i] != accept[j])
 				continue;
-				test++;
-				break;
-			}
+			test++;
+			break;
+
 		}
 		if (accept[j] == '\0')
 			break;
