fe71de8069d6dc1131d04af32a3300715fc32286
Anele Bilikwa <acehoud>
2022-06-20 13:19:23 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,8 +5,8 @@
  * any of a set of bytes
  * @s: This is the C string to be scanned
  * @accept: This is the C string containing the characters to match
- * Return: Always return 0
- * or NULL
+ * Return: pointer to the byte in s that matches one of
+ * the bytes in accept, or NULL if no such byte is found
  */
 
 char *_strpbrk(char *s, char *accept)
@@ -23,5 +23,5 @@ char *_strpbrk(char *s, char *accept)
 		s++;
 	}
 
-	return (NULL);
+	return (0);
 }
