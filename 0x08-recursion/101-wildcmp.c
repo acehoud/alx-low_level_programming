8798b05e30b7f024f19e52ee0869f02a8460d708
Anele Bilikwa <acehoud>
2022-06-23 11:29:12 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -21,5 +21,5 @@ int wildcmp(char *s1, char *s2)
 	if (*s2 == '*')
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 
-	return (*s1 == s2 ? wildcmp(s1 + 1, s2 + 1) : 0);
+	return (*s1 == *s2 ? wildcmp(s1 + 1, s2 + 1) : 0);
 }
