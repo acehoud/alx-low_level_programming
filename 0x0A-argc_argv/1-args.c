ffe91ce3f528267f403da8c831b1f99dbee9422d
Anele Bilikwa <acehoud>
2022-06-24 13:24:18 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: program that prints the number
+ * of arguments passed into it
+ * @argc: count
+ * @argv: array
+ * Return: Always return 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (args > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
