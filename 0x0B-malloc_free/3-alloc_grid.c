0634fd6d7547b192e7c5e9b481c940464711610b
Anele Bilikwa <acehoud>
2022-06-28 22:57:44 +0200

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,9 +16,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	
 	arr = malloc(sizeof(int *) * height);
-	
 	if (arr == NULL)
 		return (NULL);
 
@@ -30,7 +28,7 @@ int **alloc_grid(int width, int height)
 		{
 			for (; j >= 0; j--)
 			{
-				free(arr[j]);
+				free(arr[j])
 			}
 			free(arr);
 			return (NULL);
