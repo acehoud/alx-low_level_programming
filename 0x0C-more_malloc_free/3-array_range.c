fbe5f830086fb5fc2757423f0c823ac717e90bd9
Anele Bilikwa <acehoud>
2022-06-30 20:00:29 +0200

    3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,21 +12,19 @@
 int *array_range(int min, int max)
 {
 	int *arr = NULL; 
-	int i, j, k;
+	int i, j, count;
 
-	k = 0;
-
+	count = 0;
 	if (min > max)
 		return (NULL);
 	j = (max - min) + 1;
 	arr = malloc(j * sizeof(int));
-
 	if (arr != NULL)
 	{
 		for (i = min; i <= max; i++)
 		{
-			arr[k] = 1;
-			k++;
+			arr[count] = i;
+			count++;
 		}
 		return (arr);
 	}
