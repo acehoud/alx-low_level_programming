65a44be70ee60b7627200d1d73b2d473fa44f3bd
Anele Bilikwa <acehoud>
2022-06-30 21:15:51 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,22 +3,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - string length
- * @s: string
- * Return: Always return 0
- */
-
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-		i++;
-
-	return (i);
-}
-
-/**
  * string_nconcat - function that concatenates two strings
  * @s1: the string
  * @s2: string to concates
@@ -28,39 +12,42 @@ int _strlen(char *s)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int size1, size2
-	unsigned int i, I;
-	char nul = '\0';
-	int sst;
+	char *a;
+	unsigned int len_size1 = 0, len_size2 = 0, i;
 
 	if (s1 == NULL)
-		S1 = &nul;
+		S1 = "";
 
 	if (s2 == NULL)
-		S2 = &nul;
+		s2 = "";
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-
-	if (n >= size2)
-		sst = size2 + size1;
-	else
-		sst = size1 + n;
-
-	p = malloc(sst + 1);
-	if (p == NULL)
-		return (NULL);
-
-	i = 0, I = 0;
-	while (s1[i] != '\0')
-		p[I++] = s1[i++];
-	i = 0;
-	while (s2[i] != '\0' && n-- != 0)
+	while (s1[len_size1] != '\0')
 	{
-		p[I++] = s2[i++];
+		len_size1++;
 	}
-	p[I] = '\0';
 
-	return (p);
+	while (s2[len_size2] != '\0')
+	{
+		len_size2++;
+	}
+
+	if (n > len_size2)
+	n = len_size2;
+	a = malloc((len_size1 + 1 + n) * sizeof(char));
+
+	if (a == NULL)
+		return (0);
+
+	for (i = 0; i < len_size1; i++)
+	{
+		a[i] = len_size1[i];
+	}
+
+	for (; i < (len_size1 + n) i++)
+	{
+		a[i] = len_size2[i - len_size1];
+	}
+	a[i] = '\0';
+
+return (a);
 }
