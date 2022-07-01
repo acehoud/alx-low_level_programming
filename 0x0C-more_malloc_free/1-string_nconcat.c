1f92aaf437ae646f004bfa4a2e08fa7ce4841346
Anele Bilikwa <acehoud>
2022-07-01 05:52:49 +0200

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		a[i] = s1[i];
 	}
 
-	for (; i < (len_size1 + n) i++)
+	for (; i < (len_size1 + n); i++)
 	{
 		a[i] = s2[i - len_size1];
 	}
