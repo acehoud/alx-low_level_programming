f12bcc478c79b68f57e12a91af6a38dfd81825a2
Anele Bilikwa <acehoud>
2022-07-06 20:17:38 +0200

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,6 +3,7 @@
 /**
  * array_iterator -  function that executes a function given
  * as a parameter on each element of an array
+ * @array: array
  * @size: size of an array
  * @action: pointer to function
  *
