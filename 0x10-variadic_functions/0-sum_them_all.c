9f345a2cb369dbe40f32cc32a5ca2b3cca2dc001
Anele Bilikwa <acehoud>
2022-07-07 19:39:29 +0200

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,16 +13,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int j;
-	va_list argumnts;
+	va_list arguments;
 
 	if (n)
 	{
-		va_start(argumnts, n);
-		j = 0;
-		while (j < n)
+		va_start(arguments, n);
+		for (j = 0; j < n; j++)
 		{
-			sum += va_arg(argumnts, int);
-			j++;
+			sum += va_arg(arguments, int);
 		}
 		va_end(argumnts);
 	}
