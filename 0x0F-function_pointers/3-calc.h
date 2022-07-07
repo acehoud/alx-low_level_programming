da53021d1419448ddbf5524f0c7664027833ca4d
Anele Bilikwa <acehoud>
2022-07-07 23:52:38 +0200

    check

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -7,11 +7,13 @@
  * @op: The operator
  * @f: The function associated
  */
+
 typedef struct op
-{	
+{
 	char *op;
 	int (*f)(int a, int b);
 } op_t;
+
 int _putchar(char c);
 int op_add(int a, int b);
 int op_sub(int a, int b);
